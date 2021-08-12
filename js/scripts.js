39047a5fd41800b509d095338af2b983b064e4ce
ca2los <{ID}+{username}@users.noreply.github.com>
2021-08-12 13:56:41 -0500

    Hide and show results on search.

--- js/scripts.js
+++ js/scripts.js
@@ -91,71 +91,62 @@ function deployActor(imgUrl, actorName, resume, movieTitle, ranking, movieUrl){
 
 let api_key = '94f0c60308f2a42f4c8a0265000556cd';
 let submit_button = document.getElementById( 'submit');
+let reset_button = document.getElementById( 'reset');
 var wikiAbstract;
 
+document.body.children[1].children[0].children[1].style.display = 'none';
+
+
 function api_tmdb() {
-
-
     submit_button.addEventListener('click',function(event) {
         event.preventDefault();
 
         let input_actor = document.getElementById('name').value;
         let request_movie = 'https://api.themoviedb.org/3/search/person?api_key=' + api_key + '&language=en-US&query=' + input_actor + '&page=1&include_adult=false';
-        console.log('Results related to: ' + input_actor.toUpperCase());
 
         fetch(request_movie).then(function (response) {
             return response.json();
-        })
-            .then(function(data) {
-                for (let artist in data.results) {
-                    let id = data.results[artist].id;
-                    let name = data.results[artist].name;
-                    let known_for = data.results[artist].known_for;
-                    let popularity = data.results[artist].popularity;
-                    let profile_path = data.results[artist].profile_path;
-                    
+        }).then(function(data) {
 
-//////////////////////////
+            for (let artist in data.results) {
+                let id = data.results[artist].id;
+                let name = data.results[artist].name;
+                let known_for = data.results[artist].known_for;
+                let popularity = data.results[artist].popularity;
+                let profile_path = data.results[artist].profile_path;
 
+                let link = 'https://api.themoviedb.org/3/search/person?api_key=' + api_key + '&language=en-US&query=' + name.replace(/ /g, '%20') + '&page=1&include_adult=false';
+                console.log(link)
 
-                    let link = 'https://api.themoviedb.org/3/search/person?api_key=' + api_key + '&language=en-US&query=' + name.replace(/ /g, '%20') + '&page=1&include_adult=false';
+                let new_array = {
+                    name:name,
+                    id:id,
+                    known_for:known_for,
+                    profile_path:profile_path,
+                    link:link,
+                    popularity:popularity
+                };
+                console.log(new_array);
 
-                    let new_array = {
-                        name:name,
-                        id:id,
-                        known_for:known_for,
-                        profile_path:profile_path,
-                        link:link,
-                        popularity:popularity
-                    };
+                //Declarations for WIKI
+                var apiEndpoint = "https://en.wikipedia.org/w/api.php";
+                var params ="action=query&prop=extracts&format=json&origin=*&exsentences=3&exlimit=1&titles="+name;
 
-//////////////////
+                fetch(apiEndpoint + "?" + params).then(function (response){
+                    return response.json();
+                }).then(function(data) {
+                    var result=Object.keys(data.query.pages)[0];
+                    wikiAbstract=data.query.pages[result].extract;
+                    deployActor(profile_path, name, wikiAbstract, known_for, popularity, link);
+                });
+            }
 
+            document.body.children[1].children[0].children[1].style.display = 'block';
+        });
+    });
 
-                    //Declarations for WIKI
-                    var apiEndpoint = "https://en.wikipedia.org/w/api.php";
-                    var params ="action=query&prop=extracts&format=json&origin=*&exsentences=3&exlimit=1&titles="+name;
-
-    
-                    fetch(apiEndpoint + "?" + params).then(function (response){
-                        return response.json();
-                    }).then(function(data) {
-                        
-                        var result=Object.keys(data.query.pages)[0];
-                        
-                        wikiAbstract=data.query.pages[result].extract;
-
-                        deployActor(profile_path, name, wikiAbstract, known_for, popularity, link);
-
-            
-                    });
-
-///////////////////////
-                }
-            });
+    reset_button.addEventListener('click',function(event) {
+        document.getElementById("results").reset();
     });
 }
-
-
-
 api_tmdb();
